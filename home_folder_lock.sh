08dfa76845fe526567a59d295c886d1fbd981ac6
Ralph deGennaro <rdegennaro@sscps.org>
2014-02-07 11:36:36 -0500

    Update home_folder_lock.sh
    Modified explanation and fixed from chmod arguments.

--- home_folder_lock.sh
+++ home_folder_lock.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 # Bash skips errors and resumes by default
 
-# Check if user is in AD domain.   
-# If user is in domain and NOT in "SG_Policy_MacWriteLocal" AD group
-# then mark local profile folders read-only
-# also make sure folders are writable if membership changes
+# Check if user is in AD groups of "Domain Users" and "SG_Policy_MacWriteLocal".
+# The idea is that Domain Users should only write to networked path, unless they
+# have permission to write locally.  This is helpful for students or non-techie
+# faculty and staff.
+
+# Literal logic:
+# If user is in both AD groups, sets specific directories to u+rwX.
+#    This is done in case the membership of "SG_Policy_MacWriteLocal" changed.  
+# If the user is then a member of "Domain Users", but not SG group, sets specific directories to a-rwX.
+#    This prevents them from leaving files on local machine since they are not assigned to individuals.
+# If the users is no in "Domain Users", sets specific directories to u+rwX.
+#    This is standard Mac OS X permissions.  Done mostly in case local user is created with same name
+#    as domain users.
+# The instances where they are not in "Domain Users" but in SG group should not exist.  
 
 # Logging setup
 # vLogFileName="folder_lock.log"
@@ -25,30 +35,30 @@ if [[ $groups =~ "Domain Users" ]]; then
 #	echo $vDateTimeMMDDHHMMSS $USER "is in the domain" >>$vLogFileOutput
 	if [[ $groups =~ "SG_Policy_MacWriteLocal" ]]; then
 #		echo $vDateTimeMMDDHHMMSS $USER "is in SG_Policy_MacWriteLocal group, unlocking folders" >>$vLogFileOutput
-		chmod a+rxw /users/$USER/Documents
-		chmod a+rxw /users/$USER/Movies
-		chmod a+rxw /users/$USER/Music
-		chmod a+rxw /users/$USER/Pictures
-		chmod a+rxw /users/$USER/Downloads
-		chmod a+rxw /users/$USER/Desktop
-		chmod a+rxw /users/$USER/Public
+		chmod u+rwX /users/$USER/Documents
+		chmod u+rwX /users/$USER/Movies
+		chmod u+rwX /users/$USER/Music
+		chmod u+rwX /users/$USER/Pictures
+		chmod u+rwX /users/$USER/Downloads
+		chmod u+rwX /users/$USER/Desktop
+		chmod u+rwX /users/$USER/Public
 	else
 #		echo $vDateTimeMMDDHHMMSS $USER "is NOT in the SG_Policy_MacWriteLocal group, locking folders" >>$vLogFileOutput
-		chmod a-rxw /users/$USER/Documents
-		chmod a-rxw /users/$USER/Movies
-		chmod a-rxw /users/$USER/Music
-		chmod a-rxw /users/$USER/Pictures
-		chmod a-rxw /users/$USER/Downloads
-		chmod a-rxw /users/$USER/Desktop
-		chmod a-rxw /users/$USER/Public
+		chmod a-rwX /users/$USER/Documents
+		chmod a-rwX /users/$USER/Movies
+		chmod a-rwX /users/$USER/Music
+		chmod a-rwX /users/$USER/Pictures
+		chmod a-rwX /users/$USER/Downloads
+		chmod a-rwX /users/$USER/Desktop
+		chmod a-rwX /users/$USER/Public
 	fi
 else
 #		echo $vDateTimeMMDDHHMMSS $USER "is NOT in the domain, unlocking folders." >>$vLogFileOutput
- 		chmod a+rxw /users/$USER/Documents
-                chmod a+rxw /users/$USER/Movies
-                chmod a+rxw /users/$USER/Music
-                chmod a+rxw /users/$USER/Pictures
-                chmod a+rxw /users/$USER/Downloads
-                chmod a+rxw /users/$USER/Desktop
-                chmod a+rxw /users/$USER/Public
+ 		chmod u+rwX /users/$USER/Documents
+                chmod u+rwX /users/$USER/Movies
+                chmod u+rwX /users/$USER/Music
+                chmod u+rwX /users/$USER/Pictures
+                chmod u+rwX /users/$USER/Downloads
+                chmod u+rwX /users/$USER/Desktop
+                chmod u+rwX /users/$USER/Public
 fi
