6f7cc83dea037286a32f7359e9729b4082f3abd1
Joe McSheffrey <jmcsheffrey@sscps.org>
2015-07-06 08:08:53 -0400

    Create reset_chrome.sh

---
+++ reset_chrome.sh
@@ -0,0 +1,4 @@
+# Deletes Chrome Application Support folder to remove Chrome user data
+
+rm -r /Users/student/Library/Application\ Support\Google\Chrome
+rm -r /Users/teacher/Library/Application\ Support\Google\Chrome
